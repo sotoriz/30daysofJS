4fc869f43f610788860c96aeec1bcc4d1b35fae7
sotoriz <sotorisfx@gmail.com>
2023-07-20 05:45:16 -0700

    datatype

---
+++ Day_01/datatypes.js
@@ -0,0 +1,10 @@
+
+Q5
+//JavaScript typeof operator to check different data types.
+
+console.log(typeof 18) //numbber
+console.log(typeof 'princess') //string
+console.log(typeof True) //boolean
+console.log(typeof null) //null
+console.log(typeof undefined) //undefined
+
