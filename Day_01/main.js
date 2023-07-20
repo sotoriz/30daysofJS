d014dfba687797862b17a0f20871f887340ecef8
sotoriz <sotorisfx@gmail.com>
2023-07-20 05:48:11 -0700

    main file

---
+++ Day_01/main.js
@@ -0,0 +1,6 @@
+// the variable values can be accessed from other variable.js file
+
+console.log(firstName, lastName,country,city,  isStudent, noOfStudent, costOfSchoolBag)
+console.log(firstName, lastName, isMarried, country, age)
+console.log(age, gravity, radiusOfEarth,roomTemp) // 25, 10, 6400, 37
+console.log(myAge, yourAge)
