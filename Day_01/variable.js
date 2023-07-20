ffd92593e33af02c3ec4335c2f710df5a6b97dda
sotoriz <sotorisfx@gmail.com>
2023-07-20 05:48:54 -0700

    variablefile

---
+++ Day_01/variable.js
@@ -0,0 +1,17 @@
+
+//declare variables and assign string, boolean, undefined and null data types
+
+
+let firstName = 'Princess'//of student
+
+let lastName = 'George'//of student
+
+let country = 'Canada'//of student
+
+let city = 'Ottawa'//capital
+
+let isStudent = True
+
+let noOfStuent = undefined//not defined
+
+let costOfSchoolBag = null//empty value
