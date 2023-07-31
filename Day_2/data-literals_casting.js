ee77cd763f0af4862840ea732ac2d3aa6fda328c
sotoriz <sotorisfx@gmail.com>
2023-07-31 18:27:38 +0000

    string and literal

--- Day_2/data-literals_casting.js
+++ Day_2/data-literals_casting.js
@@ -198,4 +198,39 @@ console.log('5\t1\t5\t25\t125')
 console.log(speech.substr(31,23))
 
 
+//Level 3
+
+//'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+
+let best = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+
+let myBest = 'love'
+
+let count = best.split(myBest).length - 1
+console.log(count)
+console.log(best.split(myBest))
+
+//Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+let mySpeech = 'You cannot end a sentence with because because because is a conjunction'
+let myPattern = /love/gi
+console.log(mySpeech.match('love'))
+console.log(mySpeech.match(myPattern))
+
+//Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+    const myRegex = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+
+
+
+//Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+let txt = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+let regEx = /\d+/gi
+console.log(txt.match(regEx))
+
+let txt1 = [5000, 10000, 15000]
+const add = (a, b) => a + b
+console.log(txt1.reduce(add))
 
